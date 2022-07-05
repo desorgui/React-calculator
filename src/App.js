75067c0245bc600bb9662098737c2c9db687fcb9
Desor Guishny <desorguishny@gmail.com>
2022-07-05 18:24:00 -0400

    Fix Eslint errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+
 class App extends PureComponent {
   render() {
     return (
