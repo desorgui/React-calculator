e090134ac2407ac6ea4ef216537a6533841ebfe8
Desor Guishny <desorguishny@gmail.com>
2022-07-05 13:06:06 -0400

    add Calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './calculator.css';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='calculatorContainer'>
+        <div className='gridContainer'>
+          <ul className="calculatorGrid">
+            <li className="gridItem result">0</li>
+            <li className="gridItem">AC</li>
+            <li className="gridItem">+/-</li>
+            <li className="gridItem">%</li>
+            <li className="gridItem sign">/</li>
+            <li className="gridItem">7</li>
+            <li className="gridItem">8</li>
+            <li className="gridItem">9</li>
+            <li className="gridItem sign">X</li>
+            <li className="gridItem">4</li>
+            <li className="gridItem">5</li>
+            <li className="gridItem">6</li>
+            <li className="gridItem sign">-</li>
+            <li className="gridItem">1</li>
+            <li className="gridItem">2</li>
+            <li className="gridItem">3</li>
+            <li className="gridItem sign">+</li>
+            <li className="gridItem button0">0</li>
+            <li className="gridItem">.</li>
+            <li className="gridItem sign">=</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Calculator;
