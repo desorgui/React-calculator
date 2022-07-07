301717e88e8295b351994431b2683baba3082a63
Desor Guishny <desorguishny@gmail.com>
2022-07-07 12:41:11 -0400

    Add calculator logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { PureComponent } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
+import calculate from './components/calculate';
 
 class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prevData: {},
+      show: '0',
+    };
+  }
+
+  handler = (event) => {
+    const buttonValue = event.target.value;
+    const { prevData } = this.state;
+    const newData = calculate(prevData, buttonValue);
+    const { total, next, operation } = newData;
+    let btnpress = '';
+    if (total) {
+      btnpress += ' '.concat(total);
+    }
+    if (operation) {
+      btnpress += ' '.concat(operation);
+    }
+    if (next) {
+      btnpress += ' '.concat(next);
+    }
+    this.setState({ prevData: newData, show: btnpress });
+  }
+
   render() {
+    const { show } = this.state;
     return (
-      <Calculator />
+      <div className="calculatorGrid">
+      <Calculator handler={this.handler} showContent={show ? show : '0'} />
+      </div>
     );
   }
 }
+
 export default App;
